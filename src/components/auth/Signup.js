935860e3c7332ad560ba1b563d7ec99640439739
megha.m <megha.m@github.com>
2018-06-07 15:29:00 +0530

    Added signup functionality

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+import * as actions from '../../actions';
+
 
 class Signup extends Component {
     onSubmit = formProps => {
-        console.log(formProps);
+        this.props.signup(formProps, () => {
+            this.props.history.push('/feature')
+        })
     }
 
     render() {
@@ -28,10 +35,20 @@ class Signup extends Component {
                         autoComplete="none"                        
                     />
                 </fieldset>
+                <div>
+                    {this.props.errorMessage}
+                </div>
                 <button> Signup </button>
             </form>
         )
     }
 }
 
-export default reduxForm({ form: 'signup'})(Signup);
+function mapStateToProps(state){
+    return { errorMessage: state.auth.errorMessage }
+}
+
+export default compose(
+    connect(mapStateToProps, actions),
+    reduxForm({ form: 'signup' })
+)(Signup);
